8f958151716e636ee4db5bc7f30bd146a9b9cbf5
Minerva <minervatbd>
2024-09-24 19:52:18 -0400

    empty functions in jump

---
+++ instr_jump.c
@@ -0,0 +1,17 @@
+#include "machine.h"
+#include "instr_jump.h"
+
+void jumpToAddress(jump_instr_t i)
+{
+
+}
+
+void callSubroutine(jump_instr_t i)
+{
+
+}
+
+void returnFromSubroutine(jump_instr_t i)
+{
+    
+}
