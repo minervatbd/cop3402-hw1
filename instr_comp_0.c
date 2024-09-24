00743df7a02832f92abd7da4f050b4b0ababc037
Minerva <minervatbd>
2024-09-24 19:46:59 -0400

    empty functions in comp 0

---
+++ instr_comp_0.c
@@ -0,0 +1,62 @@
+#include "machine.h"
+#include "instr_comp_0.h"
+
+void add(comp_instr_t i)
+{
+
+}
+
+void subtract(comp_instr_t i)
+{
+
+}
+
+void copyWord(comp_instr_t i)
+{
+
+}
+
+void bitwiseAnd(comp_instr_t i)
+{
+
+}
+
+void bitwiseOr(comp_instr_t i)
+{
+
+}
+
+void bitwiseNor(comp_instr_t i)
+{
+
+}
+
+void bitwiseXor(comp_instr_t i)
+{
+
+}
+
+void loadWord(comp_instr_t i)
+{
+
+}
+
+void storeWord(comp_instr_t i)
+{
+
+}
+
+void storeAddr(comp_instr_t i)
+{
+
+}
+
+void loadWordIndirect(comp_instr_t i)
+{
+
+}
+
+void negate(comp_instr_t i)
+{
+
+}
