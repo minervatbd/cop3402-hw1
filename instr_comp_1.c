c97b96f6005095ccbceb3d9a48f76532a3f89bc1
Minerva <minervatbd>
2024-09-24 19:49:21 -0400

    empty functions in comp 1

---
+++ instr_comp_1.c
@@ -0,0 +1,57 @@
+#include "machine.h"
+#include "instr_comp_1.h"
+
+void literalLoad(comp_instr_t i)
+{
+
+}
+
+void addRegImmediate(comp_instr_t i)
+{
+
+}
+
+void subRegImmediate(comp_instr_t i)
+{
+
+}
+
+void multiply(comp_instr_t i)
+{
+
+}
+
+void divide(comp_instr_t i)
+{
+
+}
+
+void copyHI(comp_instr_t i)
+{
+
+}
+
+void copyLO(comp_instr_t i)
+{
+
+}
+
+void shiftLeftLogical(comp_instr_t i)
+{
+
+}
+
+void shiftRightLogical(comp_instr_t i)
+{
+
+}
+
+void jump(comp_instr_t i)
+{
+
+}
+
+void callSubroutineIndirectly(comp_instr_t i)
+{
+    
+}
