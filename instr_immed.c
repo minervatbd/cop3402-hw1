e4e2b283082373737ffa5ade0596675c4ef0f2da
Minerva <minervatbd>
2024-09-24 19:51:17 -0400

    empty functions in immed

---
+++ instr_immed.c
@@ -0,0 +1,57 @@
+#include "machine.h"
+#include "instr_immed.h"
+
+void addImmed(immed_instr_t i)
+{
+
+}
+
+void andImmed(immed_instr_t i)
+{
+
+}
+
+void borImmed(immed_instr_t i)
+{
+
+}
+
+void norImmed(immed_instr_t i)
+{
+
+}
+
+void xorImmed(immed_instr_t i)
+{
+
+}
+
+void branchOnEqual(immed_instr_t i)
+{
+
+}
+
+void branchGreaterEqualThanZero(immed_instr_t i)
+{
+
+}
+
+void branchGreaterThanZero(immed_instr_t i)
+{
+
+}
+
+void branchLessEqualThanZero(immed_instr_t i)
+{
+
+}
+
+void branchLessThanZero(immed_instr_t i)
+{
+
+}
+
+void brnachNotEqual(immed_instr_t i)
+{
+    
+}
