450a4d562bd71ce832ea3c519e4bc033635b8904
joann101 <125590213+joann101@users.noreply.github.com>
2024-09-28 18:11:28 -0400

    Update instr_jump.c

--- instr_jump.c
+++ instr_jump.c
@@ -1,17 +1,21 @@
-#include "machine.h"
 #include "instr_jump.h"
+#include "machine.h"
+#include "machine_types.h"
+#include "regname.h"
+#include "stack.h"
 
 void jumpToAddress(bin_instr_t i)
 {
-
+    pc = machine_types_formAddress(pc-1, i.jump.addr);
 }
 
-void callSubroutine(bin_instr_t i)
+void callSubroutine(bin_instr_t i, Stack stack)
 {
-
+    stack.GPR[RA] = pc;
+    pc = machine_types_formAddress(pc-1, i.jump.addr);
 }
 
-void returnFromSubroutine(bin_instr_t i)
+void returnFromSubroutine(bin_instr_t i, Stack stack)
 {
-    
+    pc = stack.GPR[RA];
 }
