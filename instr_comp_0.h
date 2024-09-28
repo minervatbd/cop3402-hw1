fb0fd7d1678bfa7d0c53b803a0e233dcd1a9ff6b
joann101 <125590213+joann101@users.noreply.github.com>
2024-09-28 19:40:12 -0400

    Update instr_comp_0.h

--- instr_comp_0.h
+++ instr_comp_0.h
@@ -5,17 +5,17 @@
 #include "instruction.h"
 
 // comp functions
-extern void add(bin_instr_t i);
-extern void subtract(bin_instr_t i);
-extern void copyWord(bin_instr_t i);
-extern void bitwiseAnd(bin_instr_t i);
-extern void bitwiseOr(bin_instr_t i);
-extern void bitwiseNor(bin_instr_t i);
-extern void bitwiseXor(bin_instr_t i);
-extern void loadWord(bin_instr_t i);
-extern void storeWord(bin_instr_t i);
-extern void storeAddr(bin_instr_t i);
-extern void loadWordIndirect(bin_instr_t i);
-extern void negate(bin_instr_t i);
+extern void add(bin_instr_t i, Stack stack);
+extern void subtract(bin_instr_t i, Stack stack);
+extern void copyWord(bin_instr_t i, Stack stack);
+extern void bitwiseAnd(bin_instr_t i, Stack stack);
+extern void bitwiseOr(bin_instr_t i, Stack stack);
+extern void bitwiseNor(bin_instr_t i, Stack stack);
+extern void bitwiseXor(bin_instr_t i, Stack stack);
+extern void loadWord(bin_instr_t i, Stack stack);
+extern void storeWord(bin_instr_t i, Stack stack);
+extern void storeAddr(bin_instr_t i, Stack stack);
+extern void loadWordIndirect(bin_instr_t i, Stack stack);
+extern void negate(bin_instr_t i, Stack stack);
 
 #endif
