15f23dba6e1d970dd01fea32c8dcc048e01de820
Minerva <minervatbd>
2024-09-29 12:49:24 -0400

    off set againnn

--- machine.c
+++ machine.c
@@ -302,7 +302,7 @@ void printMode(BOFFILE bof)
 
     //reset file pointer to instruction start
     word_type pc = header.text_start_address;
-    fseek(bof.fileptr, pc, SEEK_SET);
+    fseek(bof.fileptr, pc + 32, SEEK_SET);
 
     printf("Address Instruction\n");
 
