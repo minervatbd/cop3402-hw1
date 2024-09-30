40c19f8c016d6e9cd997650a9d73b418187cea78
Lex Matthews <tanukiguernica@gmail.com>
2024-09-30 15:23:58 -0400

    began instruction cycle implementation

--- machine.h
+++ machine.h
@@ -9,16 +9,16 @@
 #define MAX_DATA_LINE_LENGTH 59
 #define DATA_SEPARATOR "        ...     "
 
-//our union (mainMemory is for instructions, all other data in stack memory, temp vars are to be deleted later)
+//main memory representation
 extern union mem_u {
 word_type words[MAX_MEMORY_SIZE];
 uword_type uwords[MAX_MEMORY_SIZE];
-bin_instr_t instrs[MAX_MEMORY_SIZE];
 } stackMemory;
 
 
 void machine(int mode, char* inputFilename);
-void init(BOFHeader header, Stack* stack);
+void init(BOFHeader header, Stack* stack, address_type* pc, uword_type* hi, uword_type* lo);
+
 //executed every machine cycle
 int invariantCheck(Stack* stack, address_type pc);
 
