5d2f49bd516733dd637b45611fcfb10d80e0c26f
Lex Matthews <tanukiguernica@gmail.com>
2024-10-01 20:55:45 -0400

    trying this out

--- instr_syscall.c
+++ instr_syscall.c
@@ -135,8 +135,9 @@ static int stdPrint(char** currentOut, int len, int adr, word_type instr){
     sprintf(*currentOut, "%8d: %-6d", adr, instr);
     printf("%s", *currentOut);
 
-    if (len + strlen(*currentOut) > MAX_DATA_LINE_LENGTH)
-        return resetLen();
-        
-    else return strlen(*currentOut);
+    // if (len + strlen(*currentOut) > MAX_DATA_LINE_LENGTH)
+    //     return resetLen();
+
+    //else 
+    return strlen(*currentOut);
 }
